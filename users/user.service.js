631e8ad8ed05c204369be7fd344c4a7e48db3a51
Sergey Pososhenko <posox2000@gmail.com>
2019-05-14 23:42:17 -0400

    adding get /users/authenticate

--- users/user.service.js
+++ users/user.service.js
@@ -2,11 +2,14 @@
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' },
+{ id: 2, username: 'sergio', password: 'test', firstName: 'Sergio', lastName: 'User' }];
 
 module.exports = {
     authenticate,
-    getAll
+    getAuthenticate,
+    getAll,
+    getTest
 };
 
 async function authenticate({ username, password }) {
@@ -14,6 +17,7 @@ async function authenticate({ username, password }) {
     if (user) {
         const token = jwt.sign({ sub: user.id }, config.secret);
         const { password, ...userWithoutPassword } = user;
+        console.log('AAAAA');
         return {
             ...userWithoutPassword,
             token
@@ -21,9 +25,20 @@ async function authenticate({ username, password }) {
     }
 }
 
+async function getAuthenticate(){
+    console.log('getTest');
+    const message = 'curl --request POST --header "Content-Type: application/json" --data \'{ "username":"test", "password":"test" }\' http://127.0.0.1:4000/users/authenticate';
+    return {'message':message};
+}
+
 async function getAll() {
     return users.map(u => {
         const { password, ...userWithoutPassword } = u;
         return userWithoutPassword;
     });
+}
+
+async function getTest(req, res,next) {
+    console.log('getTest');
+    return 'test';
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -4,6 +4,8 @@ const userService = require('./user.service');
 
 // routes
 router.post('/authenticate', authenticate);
+router.get('/authenticate', getAuthenticate);
+router.get('/test',getTest);
 router.get('/', getAll);
 
 module.exports = router;
@@ -14,8 +16,18 @@ function authenticate(req, res, next) {
         .catch(err => next(err));
 }
 
+function getAuthenticate(req, res, next) {
+    userService.getAuthenticate(req.body)
+        .then(message => res.json(message))
+        .catch(err => next(err));
+}
+
 function getAll(req, res, next) {
     userService.getAll()
         .then(users => res.json(users))
         .catch(err => next(err));
+}
+
+function getTest(req, res,next) {
+	userService.getTest(req.body).then(res.status(200).json({ message: 'test' })).catch(err => next(err));
 }
